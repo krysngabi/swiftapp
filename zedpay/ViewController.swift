c7dcbe2f4482544949130023c199959635f32961
Krys <krysngabi@gmail.com>
2020-06-09 15:51:04 -0700

    Initial Commit

---
+++ zedpay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  zedpay
+//
+//  Created by Krys Ngabi on 6/9/20.
+//  Copyright © 2020 Krys Ngabi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
