921d5e420e5994157eeded3bb350e82471b31760
Krys <krysngabi@gmail.com>
2020-06-09 17:45:35 -0700

    Swift first commit

--- zedpay/ViewController.swift
+++ zedpay/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbl1: UILabel!
+    var compteur=0
+    @IBAction func btnValider(_ sender: Any) {
+        //adding console output
+        
+        print("Button clicked...eloko")
+        compteur+=1
+        if(compteur==5){
+             lbl1.text="Button tap 5 times"
+        }else{
+        lbl1.text=String(compteur)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lbl1.text="Ambra Jordan est Magnifique❣️"
     }
 
 
